8791eaa86420efb84b5394c593cfd57993c8a5a9
Osmanys <ophernandez95@gmail.com>
2021-10-29 18:43:05 -0500

    finished test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 import App from '../App'
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -11,5 +12,13 @@ test("renders without errors", () => {
 });
 
 test("shows success message on submit with form details", () => {
-    render(App)
+    render(<App/>)
+    const cart = screen.getByText(/cart/i)
+    userEvent.click(cart)
+    const checkout = screen.getByText(/checkout/i)
+    userEvent.click(checkout)
+    const checkout2 = screen.getByRole('button')
+    userEvent.click(checkout2)
+    const message = screen.getByTestId('successMessage')
+    expect(message).toBeInTheDocument()
 });
